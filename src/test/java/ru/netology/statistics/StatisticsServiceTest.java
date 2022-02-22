50c1c0f01406ea4895b1abba07a4d9798a30aab0
natabask <natabask@yandex.ru>
2022-02-22 22:13:38 +0300

    initial commit

---
+++ src/test/java/ru/netology/statistics/StatisticsServiceTest.java
@@ -0,0 +1,32 @@
+package ru.netology.statistics;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StatisticsServiceTest {
+
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldFindMaxIncreasingIncome() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 15, 18, 24, 35, 43, 58, 66, 111, 211, 312};
+        long expected = 312;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+}
